11cb2763bdd087bf6ccbef351ee6cd2cfa55d5bd
Gaurav Verma <gaurav.verma@akqa.com>
2018-05-18 20:13:45 +0530

    Reversing an array with iteration.
    Reversing an array with iteration.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HackerRank
+
+## Hacker Rank Problem Solving :)
---
+++ src/arrays/ReverseArrayIteration.java
@@ -0,0 +1,56 @@
+package arrays;
+
+import java.io.*;
+import java.util.*;
+
+public class ReverseArrayIteration {
+
+    /*
+     * Complete the reverseArray function below.
+     */
+    static int[] reverseArray(int[] a) {
+        if (a != null) {
+            int arrLen = a.length;
+            int[] output = new int[arrLen];
+            int j = 0;
+            for (int i = arrLen - 1; i >= 0; i--) {
+                output[j] = a[i];
+                j++;
+            }
+            return output;
+        }
+        return null;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arrCount = Integer.parseInt(scanner.nextLine().trim());
+
+        int[] arr = new int[arrCount];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+
+        for (int arrItr = 0; arrItr < arrCount; arrItr++) {
+            int arrItem = Integer.parseInt(arrItems[arrItr].trim());
+            arr[arrItr] = arrItem;
+        }
+
+        int[] res = reverseArray(arr);
+
+        for (int resItr = 0; resItr < res.length; resItr++) {
+            bufferedWriter.write(String.valueOf(res[resItr]));
+
+            if (resItr != res.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+    }
+}
+
